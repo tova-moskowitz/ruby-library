f29ca00eb53a19f15c5819d994ee7081c7f58330
= <=>
2017-05-07 12:49:42 -0400

    create view for books#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+  resources :subjects
+
+#   Prefix Verb   URI Pattern                  Controller#Action
+#    books GET    /books(.:format)             books#index
+#          POST   /books(.:format)             books#create
+# new_book GET    /books/new(.:format)         books#new
+# edit_book GET    /books/:id/edit(.:format)    books#edit
+#     book GET    /books/:id(.:format)         books#show
+#          PATCH  /books/:id(.:format)         books#update
+#          PUT    /books/:id(.:format)         books#update
+#          DELETE /books/:id(.:format)         books#destroy
+# subjects GET    /subjects(.:format)          subjects#index
+#          POST   /subjects(.:format)          subjects#create
+# new_subject GET    /subjects/new(.:format)      subjects#new
+# edit_subject GET    /subjects/:id/edit(.:format) subjects#edit
+#  subject GET    /subjects/:id(.:format)      subjects#show
+#          PATCH  /subjects/:id(.:format)      subjects#update
+#          PUT    /subjects/:id(.:format)      subjects#update
+#          DELETE /subjects/:id(.:format)      subjects#destroy
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+ # id | title | price | subject_id | description | created_at
+
+ Book.destroy_all
+
+
+ Book.create({title: 'The Cat in the Hat', price: "9.95", subject_id: 1, description: "Dr. Seuss book classic", created_at: DateTime.now})
+ Book.create({title: 'The Phantom Tollbooth', price: "19.95", subject_id: 3, description: "Milo goes on an adventure", created_at: DateTime.now})
+ Book.create({title: 'A Tree Grows in Brooklyn', price: "24.99", subject_id: 5, description: "1920s New York", created_at: DateTime.now})
+ Book.create({title: 'Of Mice and Men', price: "11.95", subject_id: 2, description: "Lenny doesn't know his own strength", created_at: DateTime.now})
+ Book.create({title: 'Tuck Everlasting', price: "6.95", subject_id: 4, description: "Will Winnie drink?", created_at: DateTime.now})
